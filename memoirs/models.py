98923bf789de1374a3ddd301b2eb089791ef41af
Leo <leoamby@gmail.com>
2019-10-06 00:48:06 +0300

    create an image model with the required data information like the name, image, location and category

--- memoirs/models.py
+++ memoirs/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Image(models.Model):
+    photo = models.ImageField(upload_to = 'images',)
+    name = models.CharField(max_length=30)
+    description = models.TextField()
+    location = models.ForeignKey(location)
+    category = models.ForeignKey(category)
