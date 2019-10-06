4c2fa289cd95c15a0d5fedb88fee5219f092555a
Leo <leoamby@gmail.com>
2019-10-06 17:35:44 +0300

    make changes in the project settings for the app config and url paths

--- gallery/settings.py
+++ gallery/settings.py
@@ -31,14 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'memoirs'
-    'bootstrap3'
+    'bootstrap3',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'memoirs.apps.MemoirsConfig',
 ]
 
 MIDDLEWARE = [
--- memoirs/models.py
+++ memoirs/models.py
@@ -4,5 +4,5 @@ class Image(models.Model):
     photo = models.ImageField(upload_to = 'images/', blank=True, null=True)
     name = models.CharField(max_length=30)
     description = models.TextField()
-    location = models.ForeignKey(location)
-    category = models.ForeignKey(category)
+    # location = models.ForeignKey(location)
+    # category = models.ForeignKey(category)
