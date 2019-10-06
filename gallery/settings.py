cd87464f4ce98772b24f05610c3f5c6cab9c3278
Leo <leoamby@gmail.com>
2019-10-06 17:42:19 +0300

    create a galleria database and instantiate it in the project's settings

--- gallery/settings.py
+++ gallery/settings.py
@@ -78,7 +78,9 @@ WSGI_APPLICATION = 'gallery.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'galleria',
+        'USER': 'Moringa',
+        'PASSWORD': 'psql'
     }
 }
 
