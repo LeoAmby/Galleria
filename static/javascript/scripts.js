73b5c673863fdaf6a5ab0f5c0472a0829109bf49
Leo <leoamby@gmail.com>
2019-10-07 23:04:09 +0300

    create a javascript file

---
+++ static/javascript/scripts.js
@@ -0,0 +1,6 @@
+function copyLink(){
+    var copyText = document.getElementById("myInput");
+    copyText.querySelector();
+    document.execCommand("copy")
+    alert("Copied the Text: " + copyText.value);
+}
