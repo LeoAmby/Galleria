fe81a3734a1ac074dc3f8d666d9fea6a2047a704
Leo <leoamby@gmail.com>
2019-10-07 09:48:24 +0300

    create a setup and instance test for the image model

--- memoirs/tests.py
+++ memoirs/tests.py
@@ -4,4 +4,7 @@ from .models import Image, Location, Category
 
 class ImageTestClass(TestCase):
     def setUp(self):
-        
+        self.pic1=Image(photo = 'pic1', name = 'nice')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.pic1,Image))
