7278a187d5645bc3653c9e7d33ea35b0bf22c094
Leo <leoamby@gmail.com>
2019-10-07 10:27:38 +0300

    create three more views, travel, cuisine and family

--- memoirs/views.py
+++ memoirs/views.py
@@ -3,3 +3,13 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, 'index.html')
+
+
+def travel(request):
+    render(request, 'travel.index')
+
+def cuisine(request):
+    render(request, 'cuisine.html')
+
+def family(request):
+    render(request, 'family.html')
